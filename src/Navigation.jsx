fa328d32621c2ffc411cfb723dc41fba00e57b59
xPatiCiosek <patrycja_ciosek@hotmail.com>
2024-03-31 19:06:55 +0200

    added container for sections

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -4,25 +4,25 @@ import './Navigation.css';
 function Section(props){
     return(
         <div className='section'>
-            <span><img src={"./images/" + props.icon}  alt={props.icon} /></span>
+            <span><img className='duo-logo' src={"./images/" + props.icon}  alt={props.icon} /></span>
             <span>{props.title}</span>
-        
         </div>
     )
 }
-// export default Section;
 
 function Navigation(props){
     return(
         <div className='navigation'>
-          <img src="./images/duo_logo.png" alt="duo logo" />
-          <Section title="LEARN"/>
-          <Section title="PRACTICE"/>
-          <Section title="LEADERBOARDS"/>
-          <Section title="QUESTS"/>
-          <Section title="SHOP"/>
-          <Section title="PROFILE"/>
-          <Section title="MORE"/>
+            <img src="./images/duo_logo.png" alt="duo logo" />
+            <div className='container'>
+                <Section title="LEARN"/>
+                <Section title="PRACTICE"/>
+                <Section title="LEADERBOARDS"/>
+                <Section title="QUESTS"/>
+                <Section title="SHOP"/>
+                <Section title="PROFILE"/>
+                <Section title="MORE"/>
+            </div>
         </div>
     )
 }
