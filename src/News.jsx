057b39db8b9f2d4a04bffbaaa92ec76f14dd0722
xPatiCiosek <patrycja_ciosek@hotmail.com>
2024-04-03 15:00:33 +0200

    added mock content

--- src/News.jsx
+++ src/News.jsx
@@ -4,7 +4,10 @@ import './News.css';
 function News(){
     return(
         <div className='news'>
-
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
         </div>
     )
 }
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -4,7 +4,15 @@ import './Profile.css';
 function Profile(){
     return(
         <div className='profile'>
-
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        
         </div>
     )
 }
