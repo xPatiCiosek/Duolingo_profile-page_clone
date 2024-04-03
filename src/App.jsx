699259c4f58bff662e94701d0e60848e36662657
xPatiCiosek <patrycja_ciosek@hotmail.com>
2024-04-03 17:18:58 +0200

    added react-resp and removed News for small screens

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ import './App.css';
 import NavBar from './Navigation';
 import Profile from './Profile';
 import News from './News';
+import { useMediaQuery } from 'react-responsive';
 
 function App() {
+  const isScreenSmall = useMediaQuery({ maxWidth: 800 });
 
   return (
     <>
       <NavBar />
       <main className='main-content'>
         <Profile/>
+        {isScreenSmall ? null : (
         <News/>
+        )}
       </main>
     </>
   )
