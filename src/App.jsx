ea103b6e4b7c038065e1e0e0edecd204f5ee72cd
xPatiCiosek <patrycja_ciosek@hotmail.com>
2024-04-03 13:22:29 +0200

    created Profile component

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navigation from './Navigation'
+import Profile from './Profile'
 
 function App() {
 
   return (
     <>
-      <div className='body'>
-        <Navigation/>
+      <Navigation />
         <main className='main-content'>
-          <div className='profile'>Some content</div>
-          more content
-          <div className='news'>more more more</div>
+        <Profile/>
         </main>
-      </div>  
     </>
   )
 }
---
+++ src/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Profile.css';
+
+function Profile(){
+    return(
+        <div className='profile'>
+
+        </div>
+    )
+}
+export default Profile;
