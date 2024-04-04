6fb7500153da8a369576bd4e1887b62f332c8819
xPatiCiosek <patrycja_ciosek@hotmail.com>
2024-04-04 14:06:42 +0200

    put all main-content components into Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import NavBar from './Navigation';
 import Profile from './Profile';
-import News from './News';
-import { useMediaQuery } from 'react-responsive';
+
 
 function App() {
-  const isScreenSmall = useMediaQuery({ maxWidth: 800 });
 
   return (
     <>
       <NavBar />
-      <main className='main-content'>
-        <Profile/>
-        {isScreenSmall ? null : (
-        <News/>
-        )}
-      </main>
+      <Profile/>
     </>
   )
 }
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import './Profile.css';
 
-function Profile(){
+function MainProf(){
     return(
-        <div className='profile'>
+        <div className='main'>
             <div></div>
             <div></div>
             <div></div>
@@ -14,6 +15,29 @@ function Profile(){
             <div></div>
         
         </div>
+    )
+}
+
+function News(){
+    return(
+        <div className='news'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
+
+function Profile(){
+    const isScreenSmall = useMediaQuery({ maxWidth: 800 });
+    return(
+        <main className='main-content'>
+        <MainProf/>
+        {isScreenSmall ? null : (
+        <News/>
+        )}
+      </main>
     )
 }
 export default Profile;
