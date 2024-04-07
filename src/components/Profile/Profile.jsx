78c2810dfd1f77f3bbb77ec2b4dddaa6e783777d
xPatiCiosek <patrycja_ciosek@hotmail.com>
2024-04-07 21:53:02 +0200

    created username-section

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import { useMediaQuery } from 'react-responsive';
+import { Link } from 'react-router-dom';
 import './Profile.css';
 import News from '../News/News';
 
-function MainProf(){
+function MainProf(props){
     return(
         <div className='main'>
-            <div className='profile-pic'></div>
-            <div className='username-section'></div>
-            <div className='stat-section'></div>
-            <div className='achievment-section'></div>
+            <div className='profile-pic'><button></button></div>
+            <UsernameSection name="Pati" nickname='xgydr' joinDate='December 2023' numFollowing='10' numFollowers='7'/>
+            <section className='stat-section'></section>
+            <section className='achievment-section'></section>
         
         </div>
+    )
+}
+
+function UsernameSection(props){
+    return (
+        <section className='username-section'>
+            <h2>{props.name}</h2>
+            <p className='nickname'>{props.nickname}</p>
+            <p>Joined {props.joinDate}</p>
+            <div>
+                <span className='friends'>
+                    <span><Link className='link'>{props.numFollowing} Following</Link></span>
+                    <span><Link className='link'>{props.numFollowers} Followers</Link></span>
+                </span>
+                <span><img src="https://d35aaqx5ub95lt.cloudfront.net/images/borderlessFlags/40a9ce3dfafe484bced34cdc124a59e4.svg" alt="" /></span>
+            </div>
+        </section>
     )
 }
 
@@ -19,7 +37,7 @@ function Profile(){
     const isScreenSmall = useMediaQuery({ maxWidth: 800 });
     return(
         <main className='main-content'>
-        <MainProf/>
+        <MainProf />
         {isScreenSmall ? null : (
         <News/>
         )}
